9706e302d5a775f7f3cb45fb354dfea4cb5c5a75
Daryl Stark <daryl@dstark.nl>
2024-01-07 20:28:18 +0000

    Added unit test for the `/version` endpoint

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+"""PyTest configuration file.
+
+Contains globally used fixtures for the unit testing.
+"""
+
+import pytest
+
+from fastapi.testclient import TestClient
+
+from my_rest_api.app import app
+
+
+@pytest.fixture(scope='session')
+def api_client() -> TestClient:
+    """Returns a TestClient instance for the FastAPI application.
+
+    Returns:
+        TestClient: A TestClient instance for the FastAPI application.
+    """
+    return TestClient(app)
---
+++ tests/test_rest_api.py
@@ -0,0 +1,19 @@
+"""Test for the REST API endpoints."""
+
+from fastapi.testclient import TestClient
+from my_rest_api import __version__ as api_version
+
+
+def test_version(api_client: TestClient):
+    """Test the version endpoint.
+
+    This function sends a GET request to the '/api/version' endpoint
+    and asserts that the response status code is 200.
+
+    Args:
+        api_client: a testclient for the application.
+    """
+    response = api_client.get('/version')
+    contents = response.json()
+    assert response.status_code == 200
+    assert contents['version'] == api_version
