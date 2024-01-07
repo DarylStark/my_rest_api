f8cfa52871514add88b24cf752aeab41f74bc5fe
Daryl Stark <daryl@dstark.nl>
2024-01-07 20:12:52 +0000

    Added first initial code for `FastAPI`

---
+++ src/my_rest_api/app.py
@@ -0,0 +1,25 @@
+"""The `app` object from FastAPI.
+
+This module contains the `app` object from FastAPI. It is used to run the
+application. To start the application, you can run the following command:
+    
+        uvicorn my_rest_api.app:app
+
+You can add the `--reload` flag to the command to enable auto-reloading of the
+application when changes are detected. For example:
+
+        uvicorn my_rest_api.app:app --reload
+        
+Every part of the application is imported here. These parts are then added to
+the `app` object. This allows the application to be run from a single module.
+"""
+
+from fastapi import FastAPI
+
+from .rest_api import api_router as rest_api_router
+
+# Create the FastAPI application.
+app = FastAPI()
+
+# Add the REST API endpoints to the application.
+app.include_router(rest_api_router, tags=['REST API information'])
---
+++ src/my_rest_api/model.py
@@ -0,0 +1,22 @@
+"""Module that contains extra models for the REST API."""
+
+from pydantic import BaseModel
+
+
+class Version(BaseModel):
+    """Version information for the REST API.
+
+    Attributes:
+        version: The version of the REST API.
+        python_version: The version of Python used by the REST API.
+        internal_dependencies: A dictionary of internal dependencies
+            where the key is the name of the dependency and the value is the
+            corresponding version.
+        external_dependencies: A dictionary of external dependencies
+            where the key is the name of the dependency and the value is the
+            corresponding version.
+    """
+    version: str
+    python_version: str | None = None
+    internal_dependencies: dict[str, str] | None = None
+    external_dependencies: dict[str, str] | None = None
