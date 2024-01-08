d4670defeb1bdfa28a9001c1b634b331b4dd67d7
Daryl Stark <daryl@dstark.nl>
2024-01-08 19:55:27 +0000

    Added service username and password

--- src/my_rest_api/app_config.py
+++ src/my_rest_api/app_config.py
@@ -1,10 +1,13 @@
 """Module with the configuration model of the application."""
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
+class AppConfig(BaseSettings):
     """Configuration model of the application."""
 
     model_config = SettingsConfigDict(env_prefix='MY_REST_API_')
     database_str: str = 'sqlite:///:memory:/'
+    service_user: str = Field(default=...)
+    service_password: str = Field(default=...)
