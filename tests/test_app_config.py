bea8c53da70ee380424648730bbd8a1faf2ae33d
Daryl Stark <daryl@dstark.nl>
2024-01-08 20:23:23 +0000

    Fixed linting

--- tests/test_app_config.py
+++ tests/test_app_config.py
@@ -1,7 +1,7 @@
 """Test the config model.
 
-Needed to make sure the config model is working properly and the application can
-be started.
+Needed to make sure the config model is working properly and the application
+can be started.
 """
 
 from my_rest_api.app_config import AppConfig
@@ -17,7 +17,7 @@ def test_app_config() -> None:
     config = AppConfig()
 
     # Assert that the default values are set correctly
-    assert config.debug == False
+    assert not config.debug
     assert config.database_str == 'sqlite:///:memory:/'
     assert config.service_user == 'service.user'
     assert config.service_password == 'service_password'
