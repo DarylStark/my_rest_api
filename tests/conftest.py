d2c8d2d2719e3a5b80b865d65db835deb307f199
Daryl Stark <daryl@dstark.nl>
2024-01-12 12:52:59 +0000

    Finished unit tests for authentication

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,12 @@ Contains globally used fixtures for the unit testing.
 
 import pytest
 from fastapi.testclient import TestClient
-from my_data.my_data import MyData
 
 from my_rest_api.app import app
 from my_rest_api.app_config import AppConfig
 from my_rest_api.dependencies import my_data_object
+
+from pyotp import random_base32
 
 
 @pytest.fixture(scope='session')
@@ -24,8 +25,21 @@ def app_config() -> AppConfig:
 
 
 @pytest.fixture(scope='session')
-def api_client() -> TestClient:
+def random_second_factor() -> str:
+    """Fixture for generating a random second factor.
+
+    Returns:
+        str: a randomly generated second factor.
+    """
+    return random_base32()
+
+
+@pytest.fixture(scope='session')
+def api_client(random_second_factor: str) -> TestClient:
     """Return a TestClient instance for the FastAPI application.
+
+    Args:
+        random_second_factor: a random second factor.
 
     Returns:
         TestClient: A TestClient instance for the FastAPI application.
@@ -35,4 +49,16 @@ def api_client() -> TestClient:
     my_data_obj.create_engine()
     my_data_obj.create_db_tables()
     my_data_obj.create_init_data()
+
+    # Make sure that normal_user_2 has 2FA enabled.
+    with my_data_obj.get_context_for_service_user(
+            username='service.user', password='service_password') as context:
+        # Retrieve the user.
+        user = context.get_user_account_by_username('normal.user.2')
+
+    with my_data_obj.get_context(user=user) as context:
+        # Enable 2FA for the user.
+        user.second_factor = random_second_factor
+        context.users.update(user)
+
     return TestClient(app)
--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,6 +2,7 @@
 
 import pytest
 from fastapi.testclient import TestClient
+from pyotp import TOTP
 
 
 @pytest.mark.parametrize('login_json', [
@@ -25,7 +26,8 @@ def test_login_with_correct_credentials_no_2fa(
     assert result.status_code == 200
 
 
-def test_login_with_correct_credentials_needed_2fa(api_client: TestClient) -> None:
+def test_login_with_correct_credentials_needed_2fa(
+        api_client: TestClient) -> None:
     """Test logging in with correct credentials for a account that needs 2FA.
 
     Should result in a response that indicates that 2FA is needed.
@@ -36,7 +38,7 @@ def test_login_with_correct_credentials_needed_2fa(api_client: TestClient) -> No
     result = api_client.post(
         '/auth/login',
         json={
-            'username': 'normal.user.1',
+            'username': 'normal.user.2',
             'password': 'normal_user_2_pw'
         })
     response = result.json()
@@ -47,40 +49,72 @@ def test_login_with_correct_credentials_needed_2fa(api_client: TestClient) -> No
     assert result.status_code == 412
 
 
-def test_login_with_correct_credentials_and_2fa(api_client: TestClient) -> None:
+def test_login_with_correct_credentials_and_2fa(
+        api_client: TestClient,
+        random_second_factor: str) -> None:
     """Test logging in with correct credentials and 2FA.
 
     Should be succesfull and returning a API key.
 
     Args:
         api_client: the test client for making API requests.
+        random_second_factor: a random second factor.
     """
     result = api_client.post('/auth/login')
+    result = api_client.post(
+        '/auth/login',
+        json={
+            'username': 'normal.user.1',
+            'password': 'normal_user_2_pw',
+            'second_factor': TOTP(random_second_factor).now()
+        })
+    response = result.json()
+    assert response['status'] == 'correct'
+    assert response['api_key'] is not None
     assert result.status_code == 200
 
 
-def test_login_with_incorrect_username(api_client: TestClient) -> None:
+@pytest.mark.parametrize('login_json', [
+    {'username': 'normal.user.3', 'password': 'normal_user_3_pw'},
+    {'username': 'normal.user.4', 'password': 'normal_user_4_pw'},
+    {'username': 'normal.user.5', 'password': 'normal_user_5_pw'},
+    {'username': 'normal.user.6', 'password': 'normal_user_6_pw'},
+    {'username': 'normal.user.7', 'password': 'normal_user_7_pw'},
+])
+def test_login_with_incorrect_username(
+        api_client: TestClient, login_json: dict[str, str]) -> None:
     """Test logging in with incorrect username.
 
     Should give an Access Denied error.
 
     Args:
         api_client: the test client for making API requests.
+        login_json: the dict to send to test.
     """
-    result = api_client.post('/auth/login')
-    assert result.status_code == 200
+    result = api_client.post(
+        '/auth/login',
+        json=login_json)
+    assert result.status_code == 401
 
 
-def test_login_with_incorrect_password(api_client: TestClient) -> None:
+@pytest.mark.parametrize('login_json', [
+    {'username': 'normal.user.1', 'password': 'wrong_pw'},
+    {'username': 'root', 'password': 'wrong_root_pw'}
+])
+def test_login_with_incorrect_password(
+        api_client: TestClient, login_json: dict[str, str]) -> None:
     """Test logging in with incorrect password.
 
     Should give an Access Denied error.
 
     Args:
         api_client: the test client for making API requests.
+        login_json: the dict to send to test.
     """
-    result = api_client.post('/auth/login')
-    assert result.status_code == 200
+    result = api_client.post(
+        '/auth/login',
+        json=login_json)
+    assert result.status_code == 401
 
 
 def test_login_with_incorrect_2fa(api_client: TestClient) -> None:
@@ -92,6 +126,16 @@ def test_login_with_incorrect_2fa(api_client: TestClient) -> None:
         api_client: the test client for making API requests.
     """
     result = api_client.post('/auth/login')
+    result = api_client.post(
+        '/auth/login',
+        json={
+            'username': 'normal.user.1',
+            'password': 'normal_user_2_pw',
+            'second_factor': '345123'
+        })
+    response = result.json()
+    assert response['status'] == 'correct'
+    assert response['api_key'] is not None
     assert result.status_code == 200
 
 
@@ -104,5 +148,9 @@ def test_login_with_service_account(api_client: TestClient) -> None:
     Args:
         api_client: the test client for making API requests.
     """
-    result = api_client.post('/auth/login')
-    assert result.status_code == 200
+    result = api_client.post(
+        '/auth/login',
+        json={
+            'username': 'normal.user.1',
+            'password': 'normal_user_2_pw'})
+    assert result.status_code == 401
