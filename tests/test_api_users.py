b16b8858f80c665a1eef722d36a28f56b363950b
Daryl Stark <daryl@dstark.nl>
2024-01-24 20:33:47 +0000

    Added a test for filters in the Users API endpoint

--- tests/test_api_users.py
+++ tests/test_api_users.py
@@ -1,12 +1,13 @@
 """Test the user management of the REST API."""
 
+import pytest
 from fastapi.testclient import TestClient
 
 
 def test_retrieve_users_as_root(
         api_client: TestClient,
         random_api_token_root: str) -> None:
-    """Test the authentication status with a valid token.
+    """Test retrieving users as root.
 
     Should be succesfull.
 
@@ -25,7 +26,7 @@ def test_retrieve_users_as_root(
 def test_retrieve_users_as_normal_user(
         api_client: TestClient,
         random_api_token_normal_user: str) -> None:
-    """Test the authentication status with a valid token.
+    """Test retrieving users as a normal user.
 
     Should be succesfull.
 
@@ -40,3 +41,47 @@ def test_retrieve_users_as_normal_user(
     assert result.status_code == 200
     assert len(response) == 1
     assert response[0]['username'] == 'normal.user.1'
+
+
+@pytest.mark.parametrize('field_name, operator, value, expected_length', [
+    ('id', None, 1, 1),
+    ('id', 'ne', 1, 3),
+    ('id', 'lt', 4, 3),
+    ('id', 'gt', 1, 3),
+    ('id', 'le', 3, 3),
+    ('id', 'ge', 2, 3),
+    ('username', None, 'root', 1),
+    ('username', 'contains', 'normal', 2),
+    ('username', 'notcontains', 'service', 3),
+    ('fullname', None, 'root', 1),
+    ('fullname', 'contains', 'normal', 2),
+    ('fullname', 'notcontains', 'service', 3),
+    ('email', None, 'normal_user_1@example.com', 1),
+    ('email', 'contains', 'normal_user', 2),
+    ('email', 'notcontains', 'service', 3),
+])
+def test_retrieve_users_with_filters_as_root(
+        api_client: TestClient,
+        random_api_token_root: str,
+        field_name: str,
+        operator: str | None,
+        value: str,
+        expected_length: int) -> None:
+    """Test retrieving users with filters.
+
+    Should be succesfull.
+
+    Args:
+        api_client: the test client for making API requests.
+        random_api_token_root: a token for the request.
+    """
+    filter_argument = field_name
+    if operator:
+        filter_argument += f'-{operator}'
+    filter_argument += f'={value}'
+    result = api_client.get(
+        f'/users/users?{filter_argument}',
+        headers={'X-API-Token': random_api_token_root})
+    response = result.json()
+    assert result.status_code == 200
+    assert len(response) == expected_length
